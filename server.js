e32c9f57590d8d85b6b87a8b9de193f243a7d1c6
laynex31 <laynex31@yahoo.com>
2026-04-22 19:16:07 -0500

    fileimportest

--- server.js
+++ server.js
@@ -1,13 +1,39 @@
-import http from 'http';
+import http from "http";
+import fs from "fs/promises";
+import url from "url";
+import path from "path";
 const port = 8000;
 
-const server = http.createServer((req, res) => {
+// paths
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-
-    res.write('hello world! 123');
-    res.end();
+const server = http.createServer( async (req, res) => {
+  try {
+    if (req.method === "GET") {
+      let filePath;
+      if (req.url === "/") {
+        filePath = path.join(__dirname, "public", "index.html");
+        console.log(filePath);
+      } else if (req.url === "/about") {
+        filePath = path.join(__dirname, "public", "about.html");
+        console.log(filePath);
+      } else {
+        throw new Error("not found");
+      }
+      const data = await fs.readFile(filePath);
+      res.setHeader('content-type', 'text/html');
+      res.write(data);
+      res.end();
+    } else {
+      throw new Error("not allowed");
+    }
+  } catch (error) {
+    res.writeHead(500, { "content-type": "text/plain" });
+    res.end("server error");
+  }
 });
 
 server.listen(port, () => {
-    console.log(`server running on port ${port}`);
+  console.log(`server running on port ${port}`);
 });
