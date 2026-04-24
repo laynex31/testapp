d59057e1d580d1f4bc310e8a91637c6a64912775
laynex31 <laynex31@yahoo.com>
2026-04-24 16:25:16 -0500

    added id lookup for route

--- server2.js
+++ server2.js
@@ -15,6 +15,19 @@ const server = createServer((req, res) => {
         res.write(JSON.stringify(users));
         console.log(users);
         res.end();
+    } else if(req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET') {
+        const id = req.url.split('/')[3];
+        const user = users.find((user) => user.id ===parseInt(id));
+        if (user) {
+            res.setHeader('Content-Type', 'application/json');
+        res.write(JSON.stringify(user));
+        res.end();
+        } else  {
+            res.setHeader('Content-Type', 'application/json');
+        res.write(JSON.stringify({message: '404 not found'}));
+        res.end();
+
+        }
     } else {
         res.setHeader('Content-Type', 'application/json');
         res.write(JSON.stringify({message: '404 not found'}));
